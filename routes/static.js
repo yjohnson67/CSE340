1a9de10b693aec2229084eaa121ceb6a4b607a48
yjohnson67 <joh23004@byui.edu>
2024-09-15 20:26:27 -0500

    Express Routes

--- routes/static.js
+++ routes/static.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express');//The express package is brought into the scope of the file and assigned to a local variable.
+const router = express.Router(); //The Express "router" functionality is invoked and stored into a local variable for use. Notice the (), indicating that Router is a function.
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
+router.use(express.static("public")); //Express router is to "use" the "express.static" function, meaning this is where static resources will be found, with the public folder.
+router.use("/css", express.static(__dirname + "public/css"));//any route that contains /css is to refer to the public/css folder, which is found at the root level of the project. 
+router.use("/js", express.static(__dirname + "public/js")); //any route that contains /js is to refer to the public/js folder, which is found at the root level of the project.
+router.use("/images", express.static(__dirname + "public/images"));//any route that contains /images is to refer to the public/images folder, which is found at the root level of the project.
+//Lines 7, 8 and 9 allow you to write paths pointing to these resources easily and also to add the subfolders for images, CSS and JavaScript and have them still operate correctly.
 
-module.exports = router;
+module.exports = router;//Exports the router object, along with all of these use statements for use in other areas of the application. This is VERY IMPORTANT. 
 
 
 
--- server.js
+++ server.js
@@ -6,14 +6,27 @@
  * Require Statements
  *************************/
 const express = require("express") //imports the express package
+const expressLayouts = require('express-ejs-layouts')
 const env = require("dotenv").config()
 const app = express() //creates the "application"
 const static = require("./routes/static")
 
 /* ***********************
+ * view engine and templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
  * Routes
  *************************/
+//Notice that instead of router.use, it is now app.use, meaning that the application itself will use this resource.
 app.use(static)
+//Index route
+app.get("/", function(req, res){  //The express application will watch the "get" object, within the HTTP Request, for a particular route.  //A JavaScript function that takes the request and response objects as parameters.
+  res.render("index", {title: "Home"})//The "res" is the response object, while "render()" is an Express function that will retrieve the specified view - "index" - to be sent back to the browser.  
+})
 
 /* ***********************
  * Local Server host name and port are defined
